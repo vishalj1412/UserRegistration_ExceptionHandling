3b06731ebd121bf3bafa13fbc3a99d59b6a05e69
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-28 08:55:40 +0530

    Added: user mobile no validation.

---
+++ UserValidationTest.java
@@ -0,0 +1,38 @@
+package lambda;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+
+public class UserValidationTest {
+    @Test
+    public void givenFirstNameValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+
+        Assert.assertTrue(validation.firstNameValidation("Vishal"));
+    }
+    @Test
+    public void givenlastNameValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+
+        Assert.assertTrue(validation.lastNameValidation("Jagtap"));
+    }
+    /*
+    test case for email id.
+     */
+    @Test
+    public void givenEmailIdValidation() {
+        UserRegistrationValidation emailvalidation = new UserRegistrationValidation();
+
+        Assert.assertTrue(emailvalidation.emailValidation("mkgandhi007@gmail.com"));
+    }
+    /*
+    test case for mobile number.
+     */
+    @Test
+    public void givenMobileNumberValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+
+        Assert.assertTrue(validation.mobileNumberValidation("+918882695710"));
+    }
+    }
