1437c3c654ddb88b8d9534761cf775d1c8e3c5ff
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-28 08:46:40 +0530

    da

---
+++ UserValidationTest.java
@@ -0,0 +1,13 @@
+package bridgelabz.exception;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class UserValidationTest {
+    @Test
+    public void givenFirstNameValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+        ;
+        Assert.assertTrue(validation.firstNameValidation("Vishal"));
+    }
+    }
